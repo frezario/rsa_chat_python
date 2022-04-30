93918b66d639d6ad75a43965eb0b5a7ce846b7b1
frezario <91615650+frezario@users.noreply.github.com>
2022-04-30 17:32:11 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import time
 
 
 class Server:
@@ -9,6 +10,7 @@ class Server:
         self.port = port
         self.clients = []
         self.username_lookup = {}
+        self.user_keys = {}
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         with open("count.txt", mode='w') as file:
             file.write('0')
@@ -27,9 +29,12 @@ class Server:
             self.username_lookup[c] = username
             self.clients.append(c)
 
-            # send public key to the client 
+            # receiving client's public key to store.
 
-            # ...
+            public_key = c.recv(1024).decode()
+            public_key = tuple(map(int, public_key.split(' ')))
+            print(f"Public key received from {username}:",  public_key)
+            self.user_keys[username.split(' ')[1]] = public_key
 
             # encrypt the secret with the clients public key
 
@@ -50,12 +55,25 @@ class Server:
             client.send(msg.encode())
 
     def handle_client(self, c: socket, addr):
+        # TODO: MESSAGE INTEGRITY!
         while True:
+            receiver = c.recv(1024).decode()
+            time.sleep(0.1)
             msg = c.recv(1024)
+            # print(receiver)
             # TODO: FILTERING THE CLIENTS!
-            for client in self.clients:
-                if client != c:
-                    client.send(msg)
+            try:
+                # For the one specific receiver
+                self.clients[int(receiver)].send(msg)
+
+                for num, client in enumerate(self.clients):
+                    if client != c:
+                        client.send(f"user {self.clients.index(c)} tells user {receiver} the secret!")
+            except:
+                # For the whole community
+                for num, client in enumerate(self.clients):
+                    if client != c:
+                        client.send(msg)
 
 
 if __name__ == "__main__":
